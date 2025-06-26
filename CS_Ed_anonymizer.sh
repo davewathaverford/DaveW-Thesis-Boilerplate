67ca19e391c0c593938671e1af91514387df7736
David G. Wonnacott <davew@cs.haverford.edu>
2025-06-26 10:25:49 -0400

    committing before auto-anonymizing

--- CS_Ed_anonymizer.sh
+++ CS_Ed_anonymizer.sh
@@ -2,15 +2,17 @@
 THIS=CS_Ed_anonymizer.sh # to do ... maybe use the shell variable if I can remember it? Hopefully it won't have problematic path info?
 echo " " >> ${THIS}
 wc CS_Ed*.bib
+echo "%%  Auto-regenerating anonymous bibliography at `date`" > CS_Ed_anon.bib
 git commit -m "committing before auto-anonymizing" CS_Ed*.bib ${THIS} && cat CS_Ed.bib | \
 	sed -e '/^% TO ANONYMIZE DELETE FROM HERE.../,/^% ...TO HERE (TO ANOMYMIZE, THAT IS)/d'    | \
 	sed -e 's/_ANONYMOUS_VERSION,/,/'                                                         | \
-	cat > CS_Ed_anon.bib &&
+	cat >> CS_Ed_anon.bib &&
 wc CS_Ed*.bib
 echo " " >> ${THIS}
 git commit -m "committing after auto-anonymizing" CS_Ed*.bib ${THIS}
 
 # There may be some blank lines after this, to make sure git commit has something to do, feel free to delete, sigh
+ 
  
  
  
